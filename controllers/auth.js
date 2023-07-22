30c0e481203543d78678facd9bed7d19c79c89a1
Miguel <whytemig@gmail.com>
2023-07-22 05:54:19 -0400

    almost done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,134 +1,130 @@
 const User = require("../models/users");
 const jwt = require("jsonwebtoken");
-const validator = require('validator');
+const validator = require("validator");
 const bcrypt = require("bcrypt");
-const  myBlog  = require('../models/Blog');
-const multer = require('multer');
-const fs = require('fs');
-
-
-
+const myBlog = require("../models/Blog");
+// const multer = require('multer');
+// const fs = require('fs');
 
 // Get the webpage and render the variables to the ejs page.
 const singupGet = (req, res) => {
-  
   res.render("signup", {
     title: "My Blog",
     display: "Sign Up",
-    errorMessage:''
+    errorMessage: "",
   });
-
 };
-
 
 // Post functionality for the signin page.
-const singupPost =  async (req, res) => {
+const singupPost = async (req, res) => {
   const id = req.params._id;
   let { firstName, lastName, username, email, password } = req.body;
-  
-  password = await bcrypt.hashSync(password, 10);
-  console.log(password)
-  
-  try {
-    const user = await User.create({
-      firstName,
-      lastName,
-      username,
-      email,
-      password,
-      id
+
+
+  if(password.length < 6){
+    res.render("signup", {
+      errorMessage: 'Invalid Password',
+      title: "My Blog",
+      display: "Sign Up",
     });
-    
-   
-    console.log(password)
+  }else{
+    password = await bcrypt.hashSync(password, 10);
 
-    // After the user has been created, a web-token is given to verify the user.
-    //create jwt token
-    const token = await jwt.sign({ id }, "Mysecret", {
-      expiresIn: "2h",
-    });
-
-    console.log(`token: ${token}`)
-
-    //save token in cookie
-    res.cookie("access-token", token, { httpOnly: true, maxAge: 3600000 });
-    res.redirect("/login");
-
-  } catch (err) {
-    let text = err.message;
-
-    if (validator.isEmail(email)) {
-     res.redirect('/login')
-    } else{
-      res.render("signup", { 
-        errorMessage: text.split('user validation failed:').join(),
-        title: "My Blog",
-        display: "Sign Up"
+    try {
+      const user = await User.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+        id,
       });
-  }
+      console.log(`User Created: ${user}`)
+      res.redirect("/login");
+    } catch (err) {
+      let text = err.message;
   
+      if (validator.isEmail(email)) {
+        res.redirect("/login");
+      } else {
+        res.render("signup", {
+          errorMessage: text.split('username validation failed: '),
+          title: "My Blog",
+          display: "Sign Up",
+        });
+      }
+    }
+  }
+ 
 };
-}
 
-// Get the login page and render the variables on the login page. 
+// Get the login page and render the variables on the login page.
 const loginInGet = (req, res) => {
   res.render("login", {
     title: "My Blog",
-    display: "Login" });
+    display: "Login",
+    errorMessage: " ",
+  });
 };
 
-
-// find the user by id, compare password, then Authorize them to enter the official site. 
+// find the user by id, compare password, then Authorize them to enter the official site.
 const loginInPost = async (req, res) => {
   const id = req.params._id;
-  const {username, password} = req.body;
- 
-  try{
-  const data = await User.findOne({ username });
+  const { username, password } = req.body;
 
-  if(data){
-    const hashP = bcrypt.hashSync(data.password, 10);
-    let result = await bcrypt.compare(data.password, hashP);
+  try {
+    const data = await User.findOne({ username });
 
-    console.log(password, hashP)
+    if (data) {
+      const hashP = bcrypt.hashSync(data.password, 10);
+      let result = await bcrypt.compare(data.password, hashP);
 
-    if(result){
-      const token = await jwt.sign({ id }, "Mysecret", {
-        expiresIn: "2h",
+      if (result) {
+        const token = await jwt.sign({ id }, "Mysecret", {
+          expiresIn: "2h",
+        });
+        // Save token in cookie
+        res.cookie("access-token", token, { httpOnly: true, maxAge: 3600000 });
+
+        console.log(`{
+          Authentification: ${result}, token: ${token}
+        }`);
+        res.redirect("/blogs");
+      }
+    } else{
+      res.render("login", {
+        title: "My Blog",
+        display: "Login",
+        errorMessage: " Invalid Credentials",
       });
-      // Save token in cookie
-      res.cookie("access-token", token, { httpOnly: true, maxAge: 3600000, sameSite: 'none', secure: true });
-
-      console.log( result);
-      res.redirect('/blogs');
-    
-    }else{
-      console.log('incorrect password')
-      res.status(401).json({ error: 'Invalid credentials' });
     }
+  } catch (err) {
+    console.log(err.message);
+    res.render("login", {
+      title: "My Blog",
+      display: "Login",
+      errorMessage: " Invalid Credentials",
+    });
+   
   }
-  }catch(err){
-    console.log(err.message)
-  }
-
-}
+};
 
 const getCreateBlog = async (req, res) => {
   const { title, description, date, img } = req.body;
 
-  res.render('createblog', {
+  res.render("createblog", {
     display: "Create a Blog",
-      title,
-      description,
-      date,
-      img,
-    });
+    title,
+    description,
+    date,
+    img,
+  });
 };
 
 const postCreatedBlog = async (req, res) => {
-  const { title, description, content, date, votes  } = req.body;
+  const { title, description, content, date, votes } = req.body;
   const img = req.file.filename;
-  // console.log(img)
+
   try {
     const data = new myBlog({
       title,
@@ -136,26 +132,19 @@ const postCreatedBlog = async (req, res) => {
       content,
       date,
       votes,
-     img
+      img,
     });
-    
-    await data.save()
-    console.log(data);
 
-   
-    res.redirect('/blogs')
+    await data.save();
+    console.log(`Client's Blog : ${data}`);
 
+    res.redirect("/blogs");
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.send(err.message);
-    res.render('createblog')
+    res.render("createblog");
   }
 };
-
-
-  
-
-
 
 module.exports = {
   singupGet,
@@ -163,5 +152,5 @@ module.exports = {
   loginInGet,
   loginInPost,
   getCreateBlog,
-  postCreatedBlog
+  postCreatedBlog,
 };
--- controllers/crud.js
+++ controllers/crud.js
@@ -1,102 +1,109 @@
-const  myBlog  = require('../models/Blog');
-const upload = require('../controllers/middleware/multer');
-const fs = require('fs');
-const path = require('path');
+const myBlog = require("../models/Blog");
+const upload = require("../controllers/middleware/multer");
+const fs = require("fs");
+const path = require("path");
 
-const getGalleryBlogs = async (req, res) =>{
-    const data = await myBlog.find();
-    res.render('gallery',{
-        title: "My Blog",
-        display: "Checkout the Latest",
-        data
-    });
-
-    
+const getGalleryBlogs = async (req, res) => {
+  const data = await myBlog.find();
+  res.render("gallery", {
+    title: "My Blog",
+    display: "Checkout the Latest",
+    data,
+  });
 };
 
-const getFullViewById = async (req, res)=>{
-const id = req.params.id;
-try{
+const getFullViewById = async (req, res) => {
+  const id = req.params.id;
+  try {
     const data = await myBlog.findById(id);
-    res.render('profile', {
-        title: "My Blog",
-        display: "Full View",
-        data
+    res.render("profile", {
+      title: "My Blog",
+      display: "Full View",
+      data,
     });
-
-}catch(err){
-    console.log(err.message)
-}
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-const geteditBlog = async (req, res)=>{
-    const id = req.params.id;
+const geteditBlog = async (req, res) => {
+  const id = req.params.id;
 
-    try{
+  try {
     const data = await myBlog.findById(id);
-    res.render('edit', {
-        title: "My Blog",
-        display: "Edit The Blog",
-        data
+    res.render("edit", {
+      title: "My Blog",
+      display: "Edit The Blog",
+      data,
     });
-    }catch(err){
-        console.log(err.message)
-    }
+
+   
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-const posteditBlog = async (req, res) =>{
-    let id = req.params.id;
-    const {title, description, date, img} = req.body;
-    let new_img = '';
+const posteditBlog = async (req, res) => {
+  let id = req.params.id;
+  const { title, description, date, img, updatedAt } = req.body;
+  let new_img = "";
 
-    if(req.file){
-        new_img = req.file.filename;
-        try{
-          fs.unlinkSync(path.join('image', req.body.old_image));  
-        }catch(err){
-            console.log(err.message)
-        }
-    }else{
-        new_img = req.body.old_image;
+  if (req.file) {
+    new_img = req.file.filename;
+    try {
+      fs.unlinkSync(path.join("image", req.body.old_image));
+    } catch (err) {
+      console.log(err.message);
     }
+  } else {
+    new_img = req.body.old_image;
+  }
 
-    try{
-        const data = await myBlog.findByIdAndUpdate(id, {
-            title, 
-            description, 
-            date, 
-            img: new_img
-        });
-        res.redirect('/blogs');
-    }catch(err){
-        console.log(err.message)
-    }
-    
+  try {
+    const data = await myBlog.findByIdAndUpdate(id, {
+      title,
+      description,
+      date,
+      updatedAt: new Date,
+      img: new_img,
+    });
+    res.redirect("/blogs");
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-const getDeleteBlog = async (req, res)=>{
-    const id = req.params.id;
+const getDeleteBlog = async (req, res) => {
+  let id = req.params.id;
 
-    try{
-    const data = await myBlog.findByIdAndDelete(id);
-    res.redirect('/blogs', {
-        title: "My Blog",
-        display: "Checkout the Latest",
-        data
-    })
-    }catch(err){
-        console.log(err.message);
+  try {
+    const data = await myBlog.findByIdAndRemove(id);
+
+    if (!data) {
+    res.redender("404");
+      console.log(`Blog entry with ID: ${id}, not found.`);
     }
 
-}
-
-
-
+    // if(data.img != ''){
+    //     try{
+    //         fs.unlinkSync(path.join('image', data.img));
+    //       }catch(err){
+    //           console.log(err.message)
+    //       }
+    // }
+   
+    res.redirect('/blogs')
+    console.log(`deleted: ${data}`);
+  } catch (err) {
+    res.redender('404');
+    console.log(err.message);
+  }
+};
 
 module.exports = {
-    getGalleryBlogs,
-    getFullViewById,
-    geteditBlog,
-    posteditBlog,
-    getDeleteBlog
+  getGalleryBlogs,
+  getFullViewById,
+  geteditBlog,
+  posteditBlog,
+  getDeleteBlog,
 };
